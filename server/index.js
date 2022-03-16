f89185185ed20548145a34a21606ba96343fca73
Elvis Leite <elvis7t@gmail.com>
2022-03-15 22:31:05 -0300

    FAse 1

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ import server from "./server.js"
 import { logger } from "./util.js"
 
 server.listen(3000)
-.on("listening", () =>  logger.info(`Running a f1!!!`))
+.on("listening", () =>  logger.info(`server running at ${config.port}!!`))
 
 
